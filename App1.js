a201d91ab4cbdd12682737e99be7486fb4decdf3
sonalikhollam <129723100+sonalikhollam@users.noreply.github.com>
2023-04-25 16:11:20 +0530

    movie

---
+++ App1.js
@@ -0,0 +1,25 @@
+import Header1 from "./Components/Header1";
+import './App.css';
+import Movie from "./Components/Movie";
+
+function App1()
+{
+    return(
+        <div className="App1">
+<Header1 />
+<div className="main">
+<Movie />
+<Movie />
+<Movie />
+<Movie />
+
+</div>
+
+
+
+
+
+        </div>
+    )
+}
+export default App1;
---
+++ Movie.js
@@ -0,0 +1,19 @@
+function Movie()
+{
+    return(
+        <div className="movie" >
+
+<img src="https://m.media-amazon.com/images/M/MV5BMjIwMjE1Nzc4NV5BMl5BanBnXkFtZTgwNDg4OTA1NzM@._V1_SX300.jpg " alt=""/>
+<p>The Lion King</p>
+
+<img src="https://m.media-amazon.com/images/M/MV5BMjMzODc2NzU5MV5BMl5BanBnXkFtZTgwNTMwMTE3NjM@._V1_SX300.jpg" alt=""/>
+<p>Mowgli: Legend of the Jungle</p>
+<img src="https://m.media-amazon.com/images/M/MV5BNjgwNzAzNjk1Nl5BMl5BanBnXkFtZTgwMzQ2NjI1OTE@._V1_SX300.jpg" alt=""/>
+<p>Doctor Strange</p>
+<img src="https://m.media-amazon.com/images/M/MV5BMTU2NjA1ODgzMF5BMl5BanBnXkFtZTgwMTM2MTI4MjE@._V1_SX300.jpg" alt=""/>
+<p>John Wick</p>
+</div>
+
+    )
+}
+export default Movie;
